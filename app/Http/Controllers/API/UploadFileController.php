a0aaa60674350636ce350133d65eca4b268445fa
Beesan <bkhanfar@itgsoftware.com>
2022-01-01 23:28:01 +0200

    change storage disk

--- app/Http/Controllers/API/UploadFileController.php
+++ app/Http/Controllers/API/UploadFileController.php
@@ -82,7 +82,7 @@ class UploadFileController extends Controller {
         } else {
             $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
         }
-        $storagePath  = Storage::disk('local')->getDriver()->getAdapter()->getPathPrefix();
+        $storagePath  = Storage::disk('public')->getDriver()->getAdapter()->getPathPrefix();
         $filePath = $storagePath . "public/" .  $path;
         echo $filePath . "xxxxxxxx \n" . $storagePath . "\n";
         $spreadsheet = $reader->load($filePath);
